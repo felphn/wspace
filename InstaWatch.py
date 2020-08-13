f3daadcff5d043a508aaa43b0366bc91a36b1957
Felipe Lara <68015575+felphn@users.noreply.github.com>
2020-08-12 22:09:07 -0300

    funct release (lot of implemt, refacs and bug fix)

--- InstaWatch.py
+++ InstaWatch.py
@@ -1,108 +1,103 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver import ActionChains
-from os import name, system, getlogin
+from itertools import count
+from explicit.waiter import find_element
+from os import getlogin, name, system
 from time import sleep
 
-# May be useful -> https://medium.com/better-programming/lets-create-an-instagram-bot-to-show-you-the-power-of-selenium-349d7a6744f7
-# know issue -> after showing the following/followers dialog, we're getting only single scroll on the list.
+
+def logMeIn(driver, instUser, instPass):
+    driver.get('https://www.instagram.com/')
+    sleep(1.5)
+    findField = driver.find_element_by_xpath("//input[@name='username']")
+    findField.send_keys(instUser)
+    findField = driver.find_element_by_xpath("//input[@name='password']")
+    findField.send_keys(instPass)
+    findField.send_keys(Keys.RETURN)
+    sleep(15.0)
+    driver.get(f'https://www.instagram.com/{instUser}')
+    system('cls')
+    print(f'> Watching "{instUser}" profile.\n')
+
+
+def getFollowg(driver, instUser):
+    findField = driver.find_element_by_xpath(f"//a[@href='/{instUser}/following/']")
+    findField.click()
+    print('> Catching usernames in "Following" list ...\n')
+    sleep(2.5)
+    getFollowingCSS = 'ul div li:nth-child({}) a.notranslate'
+    for listedUsers in count(start=1, step=12):
+        for usrIndex in range(listedUsers, listedUsers+12):
+            yield find_element(driver, getFollowingCSS.format(usrIndex)).text
+        lastUserListed = find_element(driver, getFollowingCSS.format(usrIndex))
+        driver.execute_script("arguments[0].scrollIntoView()", lastUserListed)
+
+
+def getFollowr(driver, instUser):
+    driver.get(f'https://www.instagram.com/{instUser}')
+    findField = driver.find_element_by_xpath(f"//a[@href='/{instUser}/followers/']")
+    findField.click()
+    print('> Catching usernames in "Followers" list ...\n')
+    sleep(2.5)
+    getFollowersCSS = 'ul div li:nth-child({}) a.notranslate'
+    for listedUsers in count(start=1, step=12):
+        for usrIndex in range(listedUsers, listedUsers+12):
+            yield find_element(driver, getFollowersCSS.format(usrIndex)).text
+        lastUserListed = find_element(driver, getFollowersCSS.format(usrIndex))
+        driver.execute_script("arguments[0].scrollIntoView()", lastUserListed)
+
+
+def watchLists(driver, instUser, totalFollowing, totalFollowers):
+    followingList = []
+    followersList = []
+    notFollowingBack = []
+    try:
+        sleep(5.0)
+        for i, usr in enumerate(getFollowg(driver, instUser), 1):
+            #print(f'{i}- {usr}')
+            followingList.append(usr)
+            if i >= totalFollowing:
+                break
+        
+        sleep(45.0)
+        for i, usr in enumerate(getFollowr(driver, instUser), 1):
+            #print(f'{i}- {usr}')
+            followersList.append(usr)
+            if i >= totalFollowers:
+                break
+        
+        sleep(45.0)
+        print('> Iterating over lists ...\n')
+        for j in range(0, len(followingList)):
+            if followingList[j] not in followersList:
+                notFollowingBack.append(followingList[j])
+            else:
+                pass
+        
+        system('cls')
+        print('-'*34, '\n> Not following you back:\n')
+        for k in range(0, len(notFollowingBack)):
+            print(f'[{k+1}]- {notFollowingBack[k]}')
+
+    finally:
+        driver.quit()
+
 
 if name == 'nt':
     system('cls')
+    print('='*8, '<> InstaWatch <>', '='*8)
+    print(' '*7, 'written by felphn')
+    instUser = str(input('\n> Username: '))
+    instPass = str(input('> Password: '))
+    totalFollowing = int(input('\n> Num of users that you follow: '))
+    totalFollowers = int(input('> Num of users that follows you: '))
+    sleep(2.5)
+    print('-'*34, '\n> Done!\n> Starting browser ...')
+    print('-'*34)
+    driver = webdriver.Edge(executable_path=f"C:\\Users\\{getlogin()}\\Documents\\msedgedriver.exe")
+    logMeIn(driver, instUser, instPass)
+    watchLists(driver, instUser, totalFollowing, totalFollowers)
+    system('pause')
+
 else:
-    system('clear')
-
-
-class InstaWatch():
-    def __init__(self, userStr, passStr):
-        self.userStr = userStr
-        self.passStr = passStr
-        self.driver = webdriver.Edge(executable_path=f'C:\\Users\\{getlogin()}\\Documents\\msedgedriver.exe')
-    
-
-    def logMeIn(self):
-        self.driver.get('https://www.instagram.com/')
-        sleep(.5)
-        userInput = self.driver.find_element_by_xpath("//input[@name='username']")
-        passInput = self.driver.find_element_by_xpath("//input[@name='password']")
-        userInput.send_keys(self.userStr)
-        passInput.send_keys(self.passStr)
-        passInput.send_keys(Keys.RETURN)
-        sleep(3.5)
-        self.driver.get(f'https://www.instagram.com/{self.userStr}')
-
-
-    def getFollowing(self, totalFollowing):
-        statInput = self.driver.find_element_by_xpath(f"//a[@href='/{self.userStr}/following/']")
-        statInput.click()
-        sleep(2.5)
-        getFollowingList = self.driver.find_element_by_css_selector("div[role='dialog'] ul")
-        numberOfFollowInList = len(getFollowingList.find_elements_by_css_selector("li"))
-        getFollowingList.click()
-        actionChain = webdriver.ActionChains(self.driver)
-        while numberOfFollowInList < totalFollowing:
-            actionChain.key_down(Keys.SPACE).key_up(Keys.SPACE).perform()
-            numberOfFollowInList = len(getFollowingList.find_elements_by_css_selector("li"))
-        followingList = []
-
-        for user in getFollowingList.find_elements_by_css_selector("li"):
-            username = user.find_element_by_css_selector("a").get_attribute("title")
-            sleep(.5)
-            followingList.append(username)
-            if len(followingList) == totalFollowing:
-                break
-        #print(followingList)
-        return followingList
-
-
-    def getFollowers(self, totalFollowers):
-        statInput = self.driver.find_element_by_xpath(f"//a[@href='/{self.userStr}/followers/']")
-        statInput.click()
-        sleep(2.5)
-        getFollowersList = self.driver.find_elements_by_css_selector("div[role='dialog'] ul")
-        numberOfFollowInList = len(getFollowersList.find_elements_by_css_selector("li"))
-        getFollowersList.click()
-        actionChain = webdriver.ActionChains(self.driver)
-        while numberOfFollowInList < totalFollowers:
-            actionChain.key_down(Keys.SPACE).key_up(Keys.SPACE).perform()
-            numberOfFollowInList = len(getFollowersList.find_elements_by_css_selector("li"))
-        followersList = []
-
-        for user in getFollowersList.find_elements_by_css_selector("li"):
-            username = user.find_element_by_css_selector("a").get_attribute("title")
-            sleep(.5)
-            followersList.append(username)
-            if len(followersList) == totalFollowers:
-                break
-        #print(followersList)
-        return followersList
-
-    
-    def compareLists(self, followingList, followersList):
-        i = j = 0
-        notFollowingBack = []
-        while True:
-            if followingList[i] == followersList[j]:
-                i+=1
-                j+=1
-                if i == len(followingList):
-                    break
-            else:
-                j+=1
-                if j == len(followingList):
-                    notFollowingBack.append(followingList[i])
-                    i+=1
-                    j=0
-
-        for k in range(0, len(notFollowingBack)):
-            print(f'{k}- {notFollowingBack[k]}\n')
-        
-
-instUser = '******'
-instPass = '******'
-
-app = InstaWatch(instUser, instPass)
-app.logMeIn()
-followingList = app.getFollowing(999999)
-followersList = app.getFollowers(999999)
-app.compareLists(followingList, followersList)
+    raise RuntimeError
